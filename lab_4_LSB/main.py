52beedc2d3cbc334ff55de84df9885dcfdee4931
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-11-06 22:43:37 +0500

    move images and messeges to misc folder

--- lab_4_LSB/main.py
+++ lab_4_LSB/main.py
@@ -1,8 +1,8 @@
 from lsb import LSB
 
 _lsb = LSB()
-_lsb.encode('./DSC_0163.bmp', './message.txt')
-print(_lsb.decode('./DSC_0163-encoded.bmp'))
+_lsb.encode('./misc/DSC_0163.bmp', './misc/message.txt')
+print(_lsb.decode('./misc/DSC_0163-encoded.bmp'))
 
 
 
